edce18082854248fb4d36a696baf8de8f62e3965
thaag7734 <48517168+thaag7734@users.noreply.github.com>
2019-03-13 08:05:33 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# MinesweePy
+
+## What is MinesweePy?
+MinesweePy is an open-source implementation of the popular Windows game **Minesweeper**. MinesweePy was built entirely from scratch in Python 3.7 using the [Tkinter](https://docs.python.org/3/library/tkinter.html "Tkinter docs") library for graphics and most gameplay functionality.
+
+## Why was MinesweePy created?
+This was a project that I created for my programming class during my sophomore year in high school (2018-19).
+
+## Can I use some of your code in my own project(s)?
+Of course! You're welcome to use my code, as long as you give credit.
---
+++ main.py
@@ -0,0 +1,360 @@
+from tkinter import *
+from random import randint
+import time
+import pymysql
+from PIL import Image, ImageTk
+import webbrowser
+import os
+import sys
+import platform
+import const
+
+def resource_path(relative_path):
+    #Get absolute path to resource, works for dev and for PyInstaller
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+class Game:
+    def __init__(self):
+        originalFlagImage = Image.open(resource_path('flag.png'))
+        resizedFlagImage = originalFlagImage.resize((const.BLOCK_SIZE, const.BLOCK_SIZE), Image.ANTIALIAS)
+        self.OPWIN = Tk()
+        self.flagImage = ImageTk.PhotoImage(resizedFlagImage)
+        self.OPWIN.iconbitmap(resource_path('icon.ico'))
+        self.OPWIN.title('MinesweePy')
+        self.OPWIN.geometry('250x150')
+        self.OPWIN.resizable(0, 0)
+        self.OPWIN.restartButton = Button(self.OPWIN, text='New Game', command=lambda : self.startNewGame())
+        self.OPWIN.flagLabel = Label(self.OPWIN, text='Flags Left: ', font=('Helvetica', 20))
+        self.OPWIN.difficulty = StringVar()
+        self.OPWIN.difficulty.trace('w', lambda *_: self.setDifficulty(self.OPWIN.difficulty.get()))
+        self.OPWIN.difficulty.set('Easy')
+        self.OPWIN.difficultyMenu = OptionMenu(self.OPWIN, self.OPWIN.difficulty, *const.GRIDS)
+        self.OPWIN.leaderboardButton = Button(self.OPWIN, text='Show Leaderboard', command=self.displayLeaderboard)
+        self.OPWIN.pauseButton = Button(self.OPWIN, text='Pause', command=lambda : self.pauseGame())
+        self.OPWIN.difficultyMenu.pack()
+        self.OPWIN.restartButton.pack()
+        self.OPWIN.leaderboardButton.pack()
+        self.outdated = self.checkVersion()
+
+    def checkVersion(self):
+        try:
+            conn = pymysql.connect(host=const.DB_LOGIN['host'], user=const.DB_LOGIN['user'], passwd=const.DB_LOGIN['passwd'],
+                                   database=const.DB_LOGIN['msgdatabase'], port=const.DB_LOGIN['port'])
+            cursor = conn.cursor()
+            cursor.execute(f'SELECT * FROM `version_info` WHERE `version` > {const.VERSION} ORDER BY `version` DESC LIMIT 1')
+            message = cursor.fetchone()
+            cursor.close()
+            conn.close()
+            if message:
+                self.outdatedWindow = Toplevel()
+                self.outdatedWindow.iconbitmap(resource_path('error.ico'))
+                header = message[1]
+                body = message[2]
+                self.outdatedWindow.versionWarningHead = Label(self.outdatedWindow, text=header, fg='red')
+                self.outdatedWindow.versionWarningBody = Label(self.outdatedWindow, text=body, wraplength=200)
+                self.outdatedWindow.updateButton = Button(self.outdatedWindow, text='Click to Update', command=self.openUpdate)
+                self.outdatedWindow.versionWarningHead.pack()
+                self.outdatedWindow.versionWarningBody.pack()
+                self.outdatedWindow.updateButton.pack()
+                return True
+            return False
+        except pymysql.err.OperationalError:
+            self.outdatedWindow = Toplevel()
+            self.outdatedWindow.iconbitmap(resource_path('error.ico'))
+            self.outdatedWindow.noConnHead = Label(self.outdatedWindow, text=const.NO_CONN['head'], fg='red')
+            self.outdatedWindow.noConnBody = Label(self.outdatedWindow, text=const.NO_CONN['body'], wraplength=200)
+            self.outdatedWindow.noConnHead.pack()
+            self.outdatedWindow.noConnBody.pack()
+            return 'nc'
+
+    def openUpdate(self):
+        system = platform.system()
+        if system == 'Windows': os.system('start "" http://degenerat.es/minesweepy')
+        elif system == 'Darwin': os.system('open "" http://degenerat.es/minesweepy')
+        elif system == 'Linux': os.system('xdg-open "" http://degenerat.es/minesweepy')
+        else: webbrowser.open('http://degenerat.es/minesweepy')
+
+    def setDifficulty(self, difficulty):
+        self.difficulty = difficulty
+
+    def startNewGame(self):
+        self.gameOver = False
+        self.exposed = 0
+        try:
+            self.WINDOW.destroy()
+            self.WINDOW = Toplevel()
+        except AttributeError:
+            self.WINDOW = Toplevel()
+        try:
+            self.victoryWindow.destroy()
+        except AttributeError:
+            pass
+        try:
+            self.leaderboardWindow.destroy()
+        except AttributeError:
+            pass
+        self.WINDOW.iconbitmap(resource_path('icon.ico'))
+        self.WINDOW.title('MinesweePy')
+        self.WINDOW.resizable(0,0)
+        self.WINDOW.pauseText = Label(self.WINDOW, text='PAUSED', font=('Helvetica', 32), anchor=CENTER)
+        self.winx = self.OPWIN.winfo_x() + self.OPWIN.winfo_width() + 50
+        self.winy = self.OPWIN.winfo_y()
+        self.WINDOW.geometry(f'+{self.winx}+{self.winy}')
+        self.field = Field(self.difficulty, self.WINDOW)
+        self.flagsLeft = IntVar()
+        self.flagsLeft.trace('w', lambda *_: self.OPWIN.flagLabel.config(text=f'Flags Left: {self.flagsLeft.get()}'))
+        self.flagsLeft.set(self.field.MINE_COUNT)
+        self.WINDOW.bind('<ButtonRelease-1>', lambda _: self.frameClicked())
+        self.WINDOW.bind('<ButtonRelease-3>', lambda _: self.flag())
+        self.WINDOW.bind('<Escape>', lambda _: self.pauseGame())
+        self.OPWIN.flagLabel.pack()
+        self.OPWIN.pauseButton.pack()
+        self.paused = False
+        self.pausedTimes = []
+        self.timerStart = time.time() #THIS IS BAD AND I SHOULDNT BE USING IT REE
+
+    def pauseGame(self):
+        if not self.paused:
+            self.paused = True
+            self.pausedTimes += [time.time() - self.timerStart]
+            for y in range(0, self.field.GRID_SIZE):
+                for x in range(0, self.field.GRID_SIZE):
+                    self.field.dispField[y][x].grid_forget()
+            self.WINDOW.pauseText.place(relx=0.5, rely=0.5, anchor=CENTER)
+        else:
+            self.paused = False
+            self.WINDOW.pauseText.place_forget()
+            for y in range(0, self.field.GRID_SIZE):
+                for x in range(0, self.field.GRID_SIZE):
+                    self.field.dispField[y][x].grid(row=y, column=x)
+            self.timerStart = time.time()
+            
+
+    def frameClicked(self):
+        if not self.gameOver and not self.paused:
+            self.visitedLastClick = []
+            y = (self.WINDOW.winfo_pointery() - self.WINDOW.winfo_rooty()) // self.field.BLOCK_SIZE
+            x = (self.WINDOW.winfo_pointerx() - self.WINDOW.winfo_rootx()) // self.field.BLOCK_SIZE
+
+            if not self.field.dispField[y][x].flagLabel.cget('image'):
+                self.expose([y,x])
+
+            if self.exposed == (self.field.GRID_SIZE ** 2) - self.field.MINE_COUNT:
+                return self.win()
+
+    def expose(self, coords):
+        self.chainEnd = False
+        y = coords[0]
+        x = coords[1]
+        if self.field.valField[y][x]:
+            return self.explode()
+
+        if self.field.dispField[y][x].cget('relief') == RAISED and not self.field.dispField[y][x].flagLabel.cget('image'):
+            self.field.dispField[y][x].config(relief=SUNKEN)
+            self.exposed += 1
+
+        if self.field.dispField[y][x].flagLabel.cget('image'):
+            self.chainEnd = True
+
+        if self.field.dispField[y][x].exposeLabel.cget('text') != '0' and not self.field.dispField[y][x].flagLabel.cget('image'):
+            self.field.dispField[y][x].exposeLabel.pack()
+            self.chainEnd = True
+
+        if [y,x] not in self.visitedLastClick:
+            self.visitedLastClick.append([y,x])
+            if not self.chainEnd:
+                if y >= 1:
+                    if not self.field.valField[y-1][x]: self.expose([y-1, x])
+                if x >= 1:
+                    if not self.field.valField[y][x-1]: self.expose([y, x-1])
+                if x < self.field.GRID_SIZE - 1:
+                    if not self.field.valField[y][x+1]: self.expose([y, x+1])
+                if y < self.field.GRID_SIZE - 1:
+                    if not self.field.valField[y+1][x]: self.expose([y+1, x])
+
+    def flag(self):
+        if not self.gameOver:
+            y = (self.WINDOW.winfo_pointery() - self.WINDOW.winfo_rooty()) // self.field.BLOCK_SIZE
+            x = (self.WINDOW.winfo_pointerx() - self.WINDOW.winfo_rootx()) // self.field.BLOCK_SIZE
+            if self.field.dispField[y][x].cget('relief') != SUNKEN:
+                if self.field.dispField[y][x].flagLabel.cget('image'):
+                    self.field.dispField[y][x].flagLabel.config(image='')
+                    self.field.dispField[y][x].flagLabel.pack_forget()
+                    self.flagsLeft.set(self.flagsLeft.get() + 1)
+                else:
+                    if self.flagsLeft.get() > 0:
+                        self.field.dispField[y][x].flagLabel.config(image=self.flagImage)
+                        self.field.dispField[y][x].flagLabel.pack()
+                        self.flagsLeft.set(self.flagsLeft.get() - 1)
+
+    def explode(self):
+        for y in range(0, self.field.GRID_SIZE):
+            for x in range(0, self.field.GRID_SIZE):
+                if self.field.valField[y][x]:
+                    self.field.dispField[y][x].config(relief=SUNKEN)
+                    self.field.dispField[y][x].flagLabel.pack_forget()
+                    self.field.dispField[y][x].exposeLabel.pack()
+        self.gameOver = True
+
+    def win(self):
+        self.gameOver = True
+        self.timerEnd = time.time()
+        self.elapsedTime = 0
+        timeSum = 0
+        for currTime in self.pausedTimes:
+            timeSum += currTime
+        self.elapsedTime += timeSum
+        self.elapsedTime += self.timerEnd - self.timerStart
+        self.victoryWindow = Toplevel()
+        self.victoryWindow.iconbitmap(resource_path('icon.ico'))
+        self.victoryWindow.scoreLabel = Label(self.victoryWindow, text='Enter your name to save your score!',
+                                              font=('Helvetica', 18), wraplength=300)
+        self.validateCmd = (self.victoryWindow.register(self.validateName), '%P', '%d', '%S')
+        self.victoryWindow.nameEntry = Entry(self.victoryWindow, validate='key', vcmd=self.validateCmd)
+        self.victoryWindow.submitButton = Button(self.victoryWindow, text='Submit',
+                                                 command=lambda : self.submitScore(self.victoryWindow.nameEntry.get(), self.elapsedTime))
+        self.victoryWindow.invalidInputLabel = Label(self.victoryWindow, wraplength=300, fg='red', text='The only allowed characters are alphanumerics, ".", " ", and "_", and the length must be within 1-255.')
+        if self.outdated == 'nc':
+            self.victoryWindow.scoreLabel.config(text=const.NO_CONN['body'], wraplength=500)
+            self.victoryWindow.nameEntry.config(state=DISABLED)
+            self.victoryWindow.submitButton.config(text='Continue')
+        elif self.outdated:
+            self.victoryWindow.scoreLabel.config(wraplength=500, text='Your program is outdated! You cannot save your scores until you update.')
+            self.victoryWindow.nameEntry.config(state=DISABLED)
+            self.victoryWindow.submitButton.config(text='Continue')
+        self.victoryWindow.scoreLabel.pack()
+        self.victoryWindow.nameEntry.pack()
+        self.victoryWindow.submitButton.pack()
+
+    def validateName(self, name, act, char):
+        if len(name) > 255: return False
+        if not (re.match('^[a-zA-Z0-9_ .]+$', name) or name == ''):
+            self.victoryWindow.invalidInputLabel.pack()
+            self.victoryWindow.submitButton.config(state=DISABLED)
+        else:
+            self.victoryWindow.invalidInputLabel.pack_forget()
+            self.victoryWindow.submitButton.config(state=NORMAL)
+        return True
+
+    def submitScore(self, name, time):
+        try:
+            if not self.outdated:
+                conn = pymysql.connect(host=const.DB_LOGIN['host'], user=const.DB_LOGIN['user'], passwd=const.DB_LOGIN['passwd'],
+                                       database=const.DB_LOGIN['database'], port=const.DB_LOGIN['port'])
+                cursor = conn.cursor()
+                numRows = cursor.execute(f'SELECT * FROM `{self.difficulty}` WHERE `name` = "{name}"')
+                if numRows >= 10:
+                    limit = numRows - 9
+                    cursor.execute(f'DELETE FROM `{self.difficulty}` WHERE `name` = "{name}" ORDER BY `time` DESC LIMIT {limit}')
+                cursor.execute(f'INSERT INTO `{self.difficulty}` (`name`,`time`) VALUES ("{name}", {time})')
+                cursor.close()
+                conn.close()
+            self.victoryWindow.destroy()
+            self.displayLeaderboard()
+        except pymysql.err.OperationalError:
+            errWindow = Toplevel()
+            errWindow.iconbitmap(resource_path('error.ico'))
+            errWindow.errHead = Label(errWindow, text=const.NO_CONN['head'], fg='red')
+            errWindow.errBody = Label(errWindow, text=const.NO_CONN['body'], wraplength=200)
+            errWindow.errHead.pack()
+            errWindow.errBody.pack()
+
+    def displayLeaderboard(self):
+        self.leaderboardWindow = Toplevel()
+        self.leaderboardWindow.iconbitmap(resource_path('icon.ico'))
+        self.leaderboardWindow.title('MinesweePy')
+        self.leaderboardWindow.titleText = Label(self.leaderboardWindow, text=f'Top times for {self.difficulty} board', font=('Helvetica', 20))
+        self.leaderboardWindow.lbFrame = Frame(self.leaderboardWindow, relief=RAISED, bd=1, highlightthickness=0, bg='#0d0d0d')
+        self.leaderboardWindow.topFive = [
+            Label(self.leaderboardWindow.lbFrame, fg='#ffd700', bg=self.leaderboardWindow.lbFrame.cget('bg'), font=('Helvetica', 20)),
+            Label(self.leaderboardWindow.lbFrame, fg='#c0c0c0', bg=self.leaderboardWindow.lbFrame.cget('bg'), font=('Helvetica', 18)),
+            Label(self.leaderboardWindow.lbFrame, fg='#cc6633', bg=self.leaderboardWindow.lbFrame.cget('bg'), font=('Helvetica', 16)),
+            Label(self.leaderboardWindow.lbFrame, fg='#eeeeee', bg=self.leaderboardWindow.lbFrame.cget('bg'), font=('Helvetica', 16)),
+            Label(self.leaderboardWindow.lbFrame, fg='#eeeeee', bg=self.leaderboardWindow.lbFrame.cget('bg'), font=('Helvetica', 16))
+            ]
+        try:
+            conn = pymysql.connect(host=const.DB_LOGIN['host'], user=const.DB_LOGIN['user'], passwd=const.DB_LOGIN['passwd'],
+                                   database=const.DB_LOGIN['database'], port=const.DB_LOGIN['port'])
+            cursor = conn.cursor()
+            cursor.execute(f'SELECT * FROM `{self.difficulty}` ORDER BY `time` LIMIT 5')
+            results = cursor.fetchall()
+            cursor.close()
+            conn.close()
+            self.leaderboardWindow.titleText.pack()
+            self.leaderboardWindow.lbFrame.pack(pady=(0, 10))
+            for scoreNum in range(0, len(results)):
+                self.leaderboardWindow.topFive[scoreNum].config(text=f'{results[scoreNum][0]}: {results[scoreNum][1]}')
+                self.leaderboardWindow.topFive[scoreNum].pack()
+        except pymysql.err.OperationalError:
+            self.leaderboardWindow.titleText.pack()
+            self.leaderboardWindow.connectionErrorText = Label(self.leaderboardWindow, text='No Connection', fg='red')
+            self.leaderboardWindow.connectionErrorText.pack()
+
+class Field:
+    def __init__(self, difficulty, window):
+        originalBombImage = Image.open(resource_path('bomb.png'))
+        resizedBombImage = originalBombImage.resize((const.BLOCK_SIZE, const.BLOCK_SIZE), Image.ANTIALIAS)
+        self.bombImage = ImageTk.PhotoImage(resizedBombImage)
+        self.BLOCK_SIZE = const.BLOCK_SIZE
+        self.GRID_SIZE  = const.GRIDS[difficulty]['GRID_SIZE']
+        self.MINE_COUNT = const.GRIDS[difficulty]['MINE_COUNT']
+        self.WINDOW = window
+
+        self.createField()
+
+    def createField(self):
+        self.valField = [[0 for i in range(0,self.GRID_SIZE)] for i in range(0,self.GRID_SIZE)]
+        
+        self.mineList = []
+        while len(self.mineList) < self.MINE_COUNT:
+            y = randint(0,self.GRID_SIZE - 1)
+            x = randint(0,self.GRID_SIZE - 1)
+            coords = [y,x]
+            if coords not in self.mineList:
+                self.mineList.append(coords)
+                self.valField[y][x] = 1
+
+        self.dispField = [[Frame(self.WINDOW, height=self.BLOCK_SIZE, width=self.BLOCK_SIZE, bd=1, highlightthickness=0, relief=RAISED) for i in range(0,self.GRID_SIZE)] for i in range(0,self.GRID_SIZE)]
+
+        for y in range(0, self.GRID_SIZE):
+            for x in range(0, self.GRID_SIZE):
+                self.dispField[y][x].exposeLabel = Label(self.dispField[y][x], font=('Helvetica', self.BLOCK_SIZE - 4))
+                self.dispField[y][x].flagLabel = Label(self.dispField[y][x], image='', font=('Helvetica', self.BLOCK_SIZE - 4))
+                self.dispField[y][x].pack_propagate(False)
+
+                if self.valField[y][x] == 1:
+                    #self.dispField[y][x].config(bg='red')
+                    self.dispField[y][x].exposeLabel.config(image=self.bombImage)
+                else:
+                    around = 0
+                    if y >= 1:
+                        if x >= 1:
+                            if self.valField[y-1][x-1]: around += 1
+                        if self.valField[y-1][x]: around += 1
+                        if x < self.GRID_SIZE - 1:
+                            if self.valField[y-1][x+1]: around += 1
+
+                    if x >= 1:
+                        if self.valField[y][x-1]: around += 1
+
+                    if x < self.GRID_SIZE - 1:
+                        if self.valField[y][x+1]: around += 1
+
+                    if y < self.GRID_SIZE - 1:
+                        if x >= 1:
+                            if self.valField[y+1][x-1]: around += 1
+                        if self.valField[y+1][x]: around += 1
+                        if x < self.GRID_SIZE - 1:
+                            if self.valField[y+1][x+1]: around += 1
+
+                    self.dispField[y][x].exposeLabel.config(text=f'{around}', fg=const.COLOURS[around])
+                self.dispField[y][x].grid(row=y, column=x)
+
+game = Game()
+game.OPWIN.mainloop()
